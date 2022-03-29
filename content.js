3494024451ef53c0712bf6d8bad8c9917d239ebb
randreu28 <randreu28@gmail.com>
2022-03-29 21:56:41 +0200

    i18n routing  & small fixes

--- content.js
+++ content.js
@@ -117,7 +117,7 @@ export const index = {
     formBtn2: "Sent",
   },
   es: {
-    title: "Hey, me llamo Rubén y yo",
+    title: "Hola, me llamo Rubén y yo",
     dynamicText: [
       "diseño experiencias",
       "pienso creativamente",
@@ -128,7 +128,7 @@ export const index = {
     introTitle: "Quién soy",
     intro1: "Hola! Mi nombre es ",
     intro2:
-      " y soy un estudiante de Multimedia de 20 años, pasional en el auto-aprendizaje y creando experiencias para la web. Estoy constantem,ente buscando nuevas oportunidades para crear productos y experiencias digitales accesibles e inclusivas.",
+      " y soy un estudiante de Multimedia de 20 años, pasional en el auto-aprendizaje y la creación de experiencias para la web. Estoy constantem,ente buscando nuevas oportunidades para crear productos y experiencias digitales accesibles e inclusivas.",
     intro3: "He trabajado en ",
     intro4: "universidades",
     intro5: "y en ",
@@ -140,82 +140,82 @@ export const index = {
     introBtn1: "Concáctame!",
     introBtn2: "Echale un vistazo a mi portfolio",
 
-    projTitle: "Some projects I've participed in",
-    projPrev: "Featured project",
-    projBtn: "See more",
-    projBtn1: "Live demo",
+    projTitle: "Algunos proyectos en los que he participado",
+    projPrev: "Proyecto destacado",
+    projBtn: "Ver más",
+    projBtn1: "Demo en vivo",
 
     proj1Title: "Imagine Planet Challenge",
     proj1: (
       <>
-        In my early years of college I got involved in a startup called{" "}
+        En los años iniciales de mi carrera, me involucré en una startup llamada{" "}
         <span className="font-bold underline decoration-green-400 decoration-wavy decoration-slice text-white">
           ecoDeliver,{" "}
         </span>
-        where I had the fortune to help as a marketing consultant and designer,
-        as I had recently learned about design and marketing principles.
+        donde tuve la oportundad de ayudar como consultor de marketing y diseñador,
+        ya que recientemente había aprendido acerca de diseño y principios de marketing.
         <br />
         <br />
-        Later on we participated and won the Imagine Planet Challenge, a
-        challenge made for newborn sustainable startups, designed for tutoring
-        and networking.
+        Después de un tiempo participamos y ganamos en Imagine Planet Challenge, 
+        un concurso en el que participaban startups sostenibles recien nacidas,
+        diseñado para tutorizar y hacer networking.
       </>
     ),
 
-    proj2Title: "Scholarship in the UPC",
+    proj2Title: "Beca de aprendizaje en la UPC",
     proj2: (
       <>
-        In the third year of college I&apos;ve been working at the Vertex centre
-        at the UPC university, where I was tasked to work on the maintenance of
-        the multiple websites of the institution.
+        En el tercer año de mi carrera, estuve trabajando en el edificio Vertex
+        en la universidad de la UPC, en donde se me encargó mantener las múltiples 
+        páginas web de la institución.
         <br />
-        <br />I was also tasked with setting up and monitoring all the data
-        received from{" "}
+        <br />
+        También se me encargó configurar y monitorizar todos los flujos de datos
+        que eran recividos desde{" "}
         <span className="font-bold underline decoration-yellow-400 decoration-wavy decoration-slice text-white">
           Google Analytics{" "}
         </span>{" "}
-        and making changes accordingly to ensure a good UX.{" "}
+        y en hacer cambios conocorde a ellos para
+        asegurar un buen UX.{" "}
       </>
     ),
 
-    proj3Title: "GOAC multimedia technician",
+    proj3Title: "Técnico multimedia de la GOAC",
     proj3: (
       <>
-        Contrary to other careers, one of the benefits of studying a design
-        career is the opportunity to show{" "}
+        Contrario a otras carreras, uno de los beneficios de estudiar una carrera con una gran base de diseño, 
+        es la oportunidad de enseñar{" "}
         <a
           href="https://www.behance.net/RubenChiquin"
           target="_blank"
           rel="noreferrer"
           className="underline text-blue-400 duration-200 hover:opacity-75 "
         >
-          your personal projects and assignments
+          tus proyectos personales y trabajos
         </a>{" "}
-        to your friends and family. And as a result of that, a friend of a
-        friend, whom I showed a design of mine contacted me for a business
-        opportunity.
+        a tu familia y amigos. Como resultado de ello, un amigo de un amigo, a quien
+        le había enseñado mi trabajo me contactó por una oportunidad de negocios.
         <br />
         <br />
-        Such opportunity was to be part of the{" "}
+        Esa oportundad fue ser parte de la {" "}
         <span className="font-bold underline decoration-cyan-400 decoration-wavy decoration-slice text-white">
-          GOAC organisation,{" "}
+          organización de la GOAC,{" "}
         </span>
-        where I was tasked to manage the social media accounts such as their
-        Youtube channel and their blog, as well as design tasks, such as
-        banners, rollups and presentation cards.
+        en donde se me encargó manejar las diferentes cuentas de redes sociales, como su canal de Youtube
+        y su blog, así como encargos de diseño, tales como banners, rollups y tarjetas de presentación.
       </>
     ),
 
-    contactTitle: "Let's get in touch!",
-    formName: "Name",
-    formSubject: "Subject",
-    formMessage: "Message",
-    formBtn0: "Send",
-    formBtn1: "Processing",
-    formBtn2: "Sent",
+    contactTitle: "Hablemos!",
+    formName: "Nombre",
+    formSubject: "Tema",
+    formMessage: "Mensaje",
+    formBtn0: "Enviar",
+    formBtn1: "Procesando",
+    formBtn2: "Enviado",
   },
   ca: {
-    title: "Hey, em dic Rubén i jo",
+    title: "Hola, em dic Rubén i jo",
     dynamicText: [
       "dissenyo experiencies",
       "penso creativament",
@@ -226,7 +226,7 @@ export const index = {
     introTitle: "Qui soc",
     intro1: "Hey! Em dic Rubén ",
     intro2:
-      "I soc un estudiant d'universitat de multimèdia de 20 anys, apassionat per l'autoaprenentatge i la construcció de experiències per la web. Sempre busco noves oportunitats per construir productes accessibles, inclusius i experiències digital",
+      "i soc un estudiant d'universitat de multimèdia de 20 anys, apassionat per l'autoaprenentatge i la construcció de experiències per la web. Sempre busco noves oportunitats per dissenyar productes i experiències digitals accessibles i inclusius.",
     intro3: "He treballat en ",
     intro4: "universitats",
     intro5: "i en ",
@@ -236,81 +236,80 @@ export const index = {
     intro8:
       "Aqui hi son algunas de las tecnologies que he estat estudiant recentment:",
     introBtn1: "Contacta'm!",
-    introBtn2: "Mira el meu portfolio",
+    introBtn2: "Mira el meu portfoli",
 
-    projTitle: "Some projects I've participed in",
-    projPrev: "Featured project",
-    projBtn: "See more",
-    projBtn1: "Live demo",
+    projTitle: "Alguns projectes en els que he participat",
+    projPrev: "Projecte destacat",
+    projBtn: "Veure més",
+    projBtn1: "Demo en directe",
 
     proj1Title: "Imagine Planet Challenge",
     proj1: (
       <>
-        In my early years of college I got involved in a startup called{" "}
+      En els primers anys de la meva carrera, em vaig involucar amb una startup amb el nom de{" "}
         <span className="font-bold underline decoration-green-400 decoration-wavy decoration-slice text-white">
           ecoDeliver,{" "}
         </span>
-        where I had the fortune to help as a marketing consultant and designer,
-        as I had recently learned about design and marketing principles.
+        on vaig tenir la oportunitat d'ajudar com a consultor de marketing i dissenyador,
+        ja que recentment habia après sobre disseny i dels principis del marketing.
         <br />
         <br />
-        Later on we participated and won the Imagine Planet Challenge, a
-        challenge made for newborn sustainable startups, designed for tutoring
-        and networking.
+        Un temps desprès vem participar i guanyar el Imagine Planet Challenge, una
+        competició per a startups sostenibles recent nascudas, dissenyada per a tutoritzar
+        i fer networking.
       </>
     ),
 
-    proj2Title: "Scholarship in the UPC",
+    proj2Title: "Beca d'aprenentatge a la UPC",
     proj2: (
       <>
-        In the third year of college I&apos;ve been working at the Vertex centre
-        at the UPC university, where I was tasked to work on the maintenance of
-        the multiple websites of the institution.
+        En el tercer any de la meva carrera, vaig estar treballant en el edifici Vertex
+        a la universitat de la UPC, on se'm va encarregar el manteniment de las multiples
+        pàgines web de la institució. 
         <br />
-        <br />I was also tasked with setting up and monitoring all the data
-        received from{" "}
+        <br />
+        També se'm va encarregar configurar i monitoritzar tot el fluxe de dades
+        tretes de{" "}
         <span className="font-bold underline decoration-yellow-400 decoration-wavy decoration-slice text-white">
           Google Analytics{" "}
         </span>{" "}
-        and making changes accordingly to ensure a good UX.{" "}
+        i de fer canvis en la forma corresponent a aquelles dades per a asegurar una bona UX.{" "}
       </>
     ),
 
-    proj3Title: "GOAC multimedia technician",
+    proj3Title: "Tècnic multimedia GOAC",
     proj3: (
       <>
-        Contrary to other careers, one of the benefits of studying a design
-        career is the opportunity to show{" "}
+        Contrari a altres carreras, un dels beneficis d'estudiar una carrera
+        amb una gran base de disseny es la oportunitat de ensenyar{" "}
         <a
           href="https://www.behance.net/RubenChiquin"
           target="_blank"
           rel="noreferrer"
           className="underline text-blue-400 duration-200 hover:opacity-75 "
         >
-          your personal projects and assignments
+          els teus projectes personals i treballs
         </a>{" "}
-        to your friends and family. And as a result of that, a friend of a
-        friend, whom I showed a design of mine contacted me for a business
-        opportunity.
+        als teus amics i familiars. Com a resultat d'això, un amic d'un amic,
+        a qui li vaig ensenyar el meu treball, em va contactar per a una oportunidad de negocis. 
         <br />
         <br />
-        Such opportunity was to be part of the{" "}
+        Aquesta oportunitat va ser ser part de{" "}
         <span className="font-bold underline decoration-cyan-400 decoration-wavy decoration-slice text-white">
-          GOAC organisation,{" "}
+          la organització GOAC,{" "}
         </span>
-        where I was tasked to manage the social media accounts such as their
-        Youtube channel and their blog, as well as design tasks, such as
-        banners, rollups and presentation cards.
+        on se'm va encarregar gestionar totes les comptes de les xarxes socials,
+        com el canal de Youtube i el seu blog, aixi com també tascas de disseny, com
+        a banners, rollups i targetas de presentació.
       </>
     ),
 
-    contactTitle: "Let's get in touch!",
-    formName: "Name",
-    formSubject: "Subject",
-    formMessage: "Message",
-    formBtn0: "Send",
-    formBtn1: "Processing",
-    formBtn2: "Sent",
+    contactTitle: "Parlem!",
+    formName: "Nom",
+    formSubject: "Tema",
+    formMessage: "Missatge",
+    formBtn0: "Enviar",
+    formBtn1: "Tramitant",
+    formBtn2: "Enviat",
   },
 };
-/* Ca and Es still to be done */
