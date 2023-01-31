21511c4aab2b720a4a16d8da21add08d7b3d832d
Rubén Chiquin <randreu28@gmail.com>
2023-01-31 13:17:32 +0100

    Adds glsl import support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  /* For Three.js glsl importation */
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
