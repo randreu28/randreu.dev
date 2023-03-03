3f07fc44fd158cc6711e56d82add54ba7d6569e9
Rubén Chiquin <randreu28@gmail.com>
2023-03-03 16:52:59 +0100

    Adds a contact section
    Also removes data-aos attributes that were used before

---
+++ src/components/Form.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useRef } from "react";
+import { useForm } from "@formcarry/react";
+
+export default function Form() {
+  const formElement = useRef<HTMLFormElement>(null!);
+
+  const { state, submit } = useForm({
+    id: "e8938LqnpHt",
+  });
+
+  useEffect(() => {
+    if (state.submitted) {
+      formElement.current.reset();
+    }
+  }, [state]);
+
+  return (
+    <form
+      ref={formElement}
+      data-aos="fade-in"
+      data-aos-delay="250"
+      onSubmit={submit}
+      className="grid grid-cols-2 gap-4 w-full"
+      acceptCharset="UTF-8"
+    >
+      <input
+        required
+        className="bg-gray-800 placeholder-white placeholder-opacity-50 rounded p-2 text-lg col-span-2 sm:col-span-1"
+        placeholder="Name"
+        type="text"
+        name="name"
+      />
+
+      <input
+        required
+        className="bg-gray-800 placeholder-white placeholder-opacity-50 rounded p-2 text-lg col-span-2 sm:col-span-1"
+        placeholder="Email"
+        type="email"
+        name="email"
+      />
+
+      <input
+        required
+        className="bg-gray-800 placeholder-white placeholder-opacity-50 col-span-2 rounded p-2 text-lg"
+        placeholder="Subject"
+        type="text"
+        name="subject"
+      />
+
+      <textarea
+        required
+        className="bg-gray-800 placeholder-white placeholder-opacity-50 col-span-2 rounded p-2 h-36 text-lg md:h-40 lg:h-52 xl:h-56"
+        placeholder="Message"
+        typeof="text"
+        name="message"
+      />
+
+      <input type="hidden" name="_gotcha" />
+
+      <div />
+
+      <span className="relative inline-flex rounded-md justify-self-end">
+        {!state.submitted && !state.submitting ? (
+          <button
+            type="submit"
+            className="items-center px-2 py-2 text-lg leading-6 font-medium rounded-md text-gray-300 bg-gray-800 hover:text-my-orange focus:border-my-orange transition ease-in-out duration-500"
+          >
+            Submit
+          </button>
+        ) : null}
+
+        {state.submitting ? (
+          <button
+            type="button"
+            className="items-center flex px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-gray-300 bg-gray-800 hover:bg-my-orange focus:border-my-orange active:bg-my-orange transition ease-in-out duration-150 cursor-default hover:cursor-default"
+            disabled={true}
+          >
+            <svg
+              className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Sending...
+          </button>
+        ) : null}
+        {state.submitted ? (
+          <button
+            disabled
+            className="items-center px-2 py-2 text-lg leading-6 font-medium rounded-md text-gray-300 bg-gray-800 opacity-50 focus:border-my-orange transition ease-in-out duration-500"
+          >
+            Submitted
+          </button>
+        ) : null}
+
+        <span
+          className={
+            state.submitted && !state.submitting
+              ? "hidden"
+              : "flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1"
+          }
+        >
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gradient-to-r from-my-orange to-my-pink opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-gradient-to-r from-my-pink to-my-orange"></span>
+        </span>
+      </span>
+    </form>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import { Leva } from "leva";
 import Link from "next/link";
 import { FolderIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Form from "@/components/Form";
+import Footer from "@/components/Footer";
 
 export default function Index() {
   const [activeSection, setActiveSection] = useState<string>(sections[0].href);
@@ -57,11 +59,7 @@ export default function Index() {
 
       <section id={sections[1].href} className="container px-6 py-8 mx-auto">
         <div className="items-center lg:flex">
-          <div
-            data-aos="fade-right"
-            data-aos-delay="250"
-            className="lg:w-1/2 space-y-5"
-          >
+          <div className="lg:w-1/2 space-y-5">
             <h2 className="text-3xl font-bold text-gray-100 border-b-2 border-gray-500 pb-2">
               Who I am
             </h2>
@@ -85,25 +83,13 @@ export default function Index() {
               <li>Flutter</li>
             </ul>
 
-            <div className="flex flex-col md:flex-row items-start space-y-5 md:space-y-0 mt-6 -mx-2 md:space-x-5">
-              <a
-                href={"#" + sections[2].href}
-                className="bg-gradient-to-br from-my-pink to-my-orange rounded-lg px-3 py-2 font-bold ml-2 text-white"
-              >
-                Let's get in touch!
-              </a>
-              <div className="flex space-x-2 ring-gray-300 ring-2 text-gray-300 rounded-lg px-3 py-2 hover:text-my-pink hover:ring-my-pink duration-200 font-bold ml-2">
-                <Link href="/portfolio">My portfolio</Link>
-                <FolderIcon className="h-6 w-6 my-auto" />
-              </div>
+            <div className="flex space-x-2 ring-gray-300 ring-2 text-gray-300 rounded-lg px-3 py-2 hover:text-my-orange hover:ring-my-orange duration-200 font-bold w-fit">
+              <Link href="/portfolio">My portfolio</Link>
+              <FolderIcon className="h-6 w-6 my-auto" />
             </div>
           </div>
 
-          <div
-            data-aos="fade-left"
-            data-aos-delay="250"
-            className="mt-8 lg:mt-0 lg:w-1/2"
-          >
+          <div className="mt-8 lg:mt-0 lg:w-1/2">
             <div className="flex items-center justify-center lg:justify-end">
               <div className="max-w-lg">
                 <Image
@@ -121,10 +107,20 @@ export default function Index() {
 
       <section
         id={sections[2].href}
-        className="h-screen flex items-center justify-center text-5xl border-b"
+        className="space-y-10 container px-6 py-8 mx-auto"
       >
-        {sections[2].name}
+        <div className="items-center lg:flex">
+          <div className="lg:w-1/2 space-y-5">
+            <h2 className="text-3xl w-full font-bold text-white border-b-2 border-gray-500 pb-2">
+              Let's get in touch!
+            </h2>
+          </div>
+
+          <div className="mt-8 lg:mt-0 lg:w-1/2" />
+        </div>
+        <Form />
       </section>
+      <Footer />
     </>
   );
 }
